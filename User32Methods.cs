b8435a70207dd6f881f2870a200d40c5c9f1d181
Axiom <59965938+l-Axiom-l@users.noreply.github.com>
2022-02-08 20:33:44 +0100

    AxiomZeux

---
+++ User32Methods.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmulateInputTest
+{
+    class User32Methods
+    {
+        [DllImport("User32")]
+        static extern int MessageBox(IntPtr hWnd, String text, String caption, uint type);
+
+        [DllImport("user32.dll")]
+        public static extern bool SetCursorPos(int x, int y);
+
+        public static Point GetCursorPosition()
+        {
+            POINT lpPoint;
+            GetCursorPos(out lpPoint);
+            // NOTE: If you need error handling
+            // bool success = GetCursorPos(out lpPoint);
+            // if (!success)
+
+            return lpPoint;
+        }
+
+        public static void DoClickMouse(int mouseButton)
+        {
+            var input = new INPUT()
+            {
+                dwType = 0, // Mouse input
+                mi = new MOUSEINPUT() { dwFlags = mouseButton }
+            };
+
+            SendInput(1, input, Marshal.SizeOf(input));
+        }
+
+        #region Behind the Scenes
+
+        [DllImport("user32.dll")]
+        static extern void SendInput(uint cInputs, INPUT input, int size);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+
+            public static implicit operator Point(POINT point)
+            {
+                return new Point(point.X, point.Y);
+            }
+        }
+
+        struct INPUT
+        {
+            public uint dwType;
+            public MOUSEINPUT mi;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct MOUSEINPUT
+        {
+            int dx;
+            int dy;
+            int mouseData;
+            public int dwFlags;
+            int time;
+            IntPtr dwExtraInfo;
+        }
+        #endregion
+    }
+}
